2d1e1b689c4d01ad4bf0548bb51883a43a063205
Tim Peterson <petersont4@nku.edu>
2019-04-24 14:14:55 -0400

    Create dial settings.py

---
+++ dial settings.py
@@ -0,0 +1,4 @@
+jasperURL = "" #URL for reporting server
+location = "" #Name of location for statistics
+dbusername = "pi" #Username for local database connection
+dbpassword = "" #Password for local database connection
